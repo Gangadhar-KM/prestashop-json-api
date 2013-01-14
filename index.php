4f9c2b512c93ea8b76e2d67ace22717def0cb622
Jean-Philippe Bidegain <jean-philippe@kaizen-developments.com>
2013-01-14 10:12:26 +0100

    [-] Initilisation correcte de Prestashop en utilisant un FrontController

--- index.php
+++ index.php
@@ -1,28 +1,42 @@
 <?php
 
-// Prestashop Initializationx
-require_once(dirname(__FILE__).'/../../config/settings.inc.php');
-require_once(dirname(__FILE__).'/../../config/defines.inc.php');
-require_once(dirname(__FILE__).'/../../config/autoload.php');
+require(dirname(__FILE__).'/../../config/config.inc.php');
 
-Context::getContext()->shop = Shop::initialize();
-Shop::setContext(Shop::CONTEXT_SHOP, Context::getContext()->shop->id);
+class PrestashopJsonApi extends FrontController {
 
-// F3 initialization
-$f3 = require(dirname(__FILE__).'/lib/base.php');
-$f3->set('AUTOLOAD','app/controllers/');
+	public function __construct() {
+		$this->name = 'productcomments';
+		$this->f3 = require(dirname(__FILE__).'/lib/base.php');
 
-$f3->route('GET /', function() {
-	die(msgpack_pack('Prestashop JSON API'));
-});
+		parent::__construct();
+		$this->context = Context::getContext();
+	}
 
-// Router
-$f3->map('/cms/@id_cms', 'JSONApi\Controllers\CmsController');
-$f3->map('/product/@id_product'	, 'JSONApiProductController');
-$f3->map('/category/@id_category/@page'	, 'JSONApiCategoryController');
-$f3->route('GET /product/latest', 'JSONApi\Controllers\ProductController->getLatest()');
-$f3->route('GET /product/sales', 'JSONApi\Controllers\ProductController->getSales()');
-$f3->route('GET /search/@term', 'JSONApi\Controllers\SearchController->find()');
+	// Initialization
+	public function initContent() {
+		parent::initContent();
+		$this->f3->set('AUTOLOAD','app/controllers/');
 
-// Run!
-$f3->run();
+		$this->f3->route('GET /', function() {
+			die(msgpack_pack('Prestashop JSON API'));
+		});
+
+		// Router
+		$this->f3->map('/cms/@id_cms', 'JSONApi\Controllers\CmsController');
+		$this->f3->map('/product/@id_product', 'JSONApiProductController');
+		$this->f3->map('/category/@id_category/@page', 'JSONApiCategoryController');
+		$this->f3->route('GET /product/latest', 'JSONApi\Controllers\ProductController->getLatest()');
+		$this->f3->route('GET /product/sales', 'JSONApi\Controllers\ProductController->getSales()');
+		$this->f3->route('GET /search/@term', 'JSONApi\Controllers\SearchController->find()');
+	}
+
+	// Run!
+	public function run() {
+		$this->f3->run();
+	}
+}
+
+$prestashopJsonApi = new PrestashopJsonApi();
+$prestashopJsonApi->init();
+$prestashopJsonApi->initContent();
+$prestashopJsonApi->run();
